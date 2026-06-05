4fd7fd8ba66df9f82af0c9f96bb2922efdd88559
Javier020301 <javiesaransig2@gmail.com>
2026-06-05 02:16:47 -0500

    other corrections for the test

--- backend/app/routes/auth_routes.py
+++ backend/app/routes/auth_routes.py
@@ -110,7 +110,7 @@ def login(login_data: LoginRequest, db: Session = Depends(get_db)):
             },
         )
 
-
+"""
 @router.get("/list", response_model=UserListResponse, status_code=status.HTTP_200_OK)
 def get_users(role: Optional[str] = None, db: Session = Depends(get_db)):
     # Endpoint to get all users, optionally filtered by role
@@ -128,3 +128,4 @@ def get_users(role: Optional[str] = None, db: Session = Depends(get_db)):
                 "message": "Internal server error",
             },
         )
+"""
