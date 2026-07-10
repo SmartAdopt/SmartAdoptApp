c1f6ae19c028d3029e4acc3bf861e418cd4209c3
wfap <217279580+wfandrangop@users.noreply.github.com>
2026-07-10 00:23:43 -0500

    refactor(cid/cd): extract android directory form frontend module
    The android directory is modified to reside in the project root. SA-173
    
    Closes: #199

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'net.programacionwebuce.smartadopt',
+  appName: 'SmartAdopt',
+  webDir: 'frontend/dist'
+};
+
+export default config;
